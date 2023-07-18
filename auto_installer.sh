a408d09a0906d3fc7da86cf2b2f3c5636e17119c
PICTC <support@premier-ictc.com>
2023-07-18 09:18:27 +0000

    Refactor

--- auto_installer.sh
+++ auto_installer.sh
@@ -1,9 +1,26 @@
+#!/bin/bash
 
-sudo microk8s kubectl create namespace argocd
-sudo microk8s kubectl apply -n argocd -f ./installer/install.yaml
+echo "\$1 = '$1'"
+if [ "$1" -eq "" ]; then
+    KUBE_CTL = "microk8s kubectl"
+else
+    KUBE_CTL = $1
 
-# sudo microk8s kubectl apply -f ./control_plane/00-role.yml -f ./control_plane/00-account.yml -f ./control_plane/01-role-binding.yml -f ./control_plane/02-traefik.yml -f ./control_plane/02-traefik-services.yml
+
+echo "### Download Latest ArgoCD Standalone config file... \n\n"
+cd ./ns-control_plane/po-orchestrator &&  /
+    wget https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 
-sudo microk8s kubectl create namespace whoami
-sudo microk8s kubectl apply -f ./control_plane/03-whoami.yml -f ./control_plane/03-whoami-services.yml -f ./control_plane/04-whoami-ingress.yml
+echo "### Deploy 'ArgoCD Standalone' deployment into the 'control_plane' namespace... \n\n"
+cmd "$KUBE_CTL create namespace control_plane"
+cmd "$KUBE_CTL apply -n ns-control_plane -f ./ns-control_plane/po-orchestrator/install.yaml"
+
+# echo "### Create Ingress rule for 'ArgoCD Server' web UI via the FQDN... \n\n"
+# cmd "$KUBE_CTL create ingress --rule=\"cpanel.csp.cloud.premier-ictc.com/=svc1:80\" "
+
+# echo "### Deploy 'WhoAmI' pod into the 'control_plane' namespace... \n\n"
+# cmd "$KUBE_CTL create namespace whoami"
+# cmd "$KUBE_CTL apply -f ./ns-control_plane/po-testing -R"
+
+
