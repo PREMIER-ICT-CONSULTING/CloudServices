d84b3c8bebd1b23ae09d3a0bb025fb8cbf1e6496
PICTC <support@premier-ictc.com>
2023-07-18 15:59:41 +0000

    Update 'Auto Install' scripts

--- auto_installer.sh
+++ auto_installer.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
 
-echo "\$1 = '$1'"
-if [ -z "$1" ]
-then
-    KUBE_CTL = "microk8s kubectl"
-else
-    KUBE_CTL = $1
-fi
+SHORT=k,h
+LONG=kubectl-cmds,help
+OPTS=$(getopt -a -n auto_installer --options $SHORT --longoptions $LONG -- "$@")
 
-echo "KUBE_CTL = '$KUBE_CTL' "
+USAGE="    Usage:  \$ ./auto_installer.sh [ [-k|--kubectl-cmds] \"COMMANDS\" ] | [ -h|--help ]"
+
+eval set -- "$OPTS"
+
+while :
+do
+  case "$1" in
+    -k | --kubectl-cmds )
+      shift
+      KUBE_CTL="$@"
+      break
+      ;;
+    -h | --help )
+      echo ""
+      echo "Auto deploy using 'kubectl' or specify another command tool. "
+      echo ""
+      echo "$USAGE"
+      break
+      ;;
+    - | -- )
+      echo "Invalid option at '$1', Try:"
+      echo ""
+      echo "$USAGE"
+      break
+      ;;
+    * )
+      if [ -z "$@" ]
+      then
+        KUBE_CTL="kubectl"
+      else
+        echo "Invalid arguments '$@', Try:"
+        echo ""
+        echo "$USAGE"
+      fi
+      break
+      ;;
+  esac
+done
+
+echo "KUBE_CTL = '$KUBE_CTL'"
