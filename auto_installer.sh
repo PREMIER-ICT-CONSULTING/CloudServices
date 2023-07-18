b68e7a5dee6b7899678c938c3da72b444d86341b
PICTC <support@premier-ictc.com>
2023-07-18 10:26:35 +0000

    .

--- auto_installer.sh
+++ auto_installer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "\$1 = '$1'"
-if [ "$1" -eq "" ]; then
+if [ "$1" -eq " " ]
+then
     KUBE_CTL = "microk8s kubectl"
 else
     KUBE_CTL = $1
+fi
+
+echo "KUBE_CTL = '$KUBE_CTL' "
