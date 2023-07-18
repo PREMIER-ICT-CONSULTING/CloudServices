33ac1e29f0783c116278f0c50f62f4c6fd4af10f
PICTC <support@premier-ictc.com>
2023-07-18 16:08:12 +0000

    Updates

--- auto_installer.sh
+++ auto_installer.sh
@@ -2,9 +2,9 @@
 
 SHORT=k,h
 LONG=kubectl-cmds,help
-OPTS=$(getopt -a -n auto_installer --options $SHORT --longoptions $LONG -- "$@")
+OPTS=$(getopt -a -n auto_installer.sh --options $SHORT --longoptions $LONG -- "$@")
 
-USAGE="    Usage:  \$ ./auto_installer.sh [ [-k|--kubectl-cmds] \"COMMANDS\" ] | [ -h|--help ]"
+USAGE="    Usage:  \$ ./auto_installer.sh [ [-k|--kubectl-cmds] \"COMMANDS...\" ] | [ -h|--help ]"
 
 eval set -- "$OPTS"
 
@@ -12,35 +12,56 @@ while :
 do
   case "$1" in
     -k | --kubectl-cmds )
-      shift
+      shift 2;
       KUBE_CTL="$@"
+      echo ""
+      echo "Using default 'kubectl' command set!"
+      echo ""
       break
       ;;
     -h | --help )
-      echo ""
-      echo "Auto deploy using 'kubectl' or specify another command tool. "
-      echo ""
-      echo "$USAGE"
-      break
-      ;;
-    - | -- )
-      echo "Invalid option at '$1', Try:"
+      echo "Auto deployment using 'kubectl' or some other K8s ctl command tool. "
       echo ""
       echo "$USAGE"
-      break
+      echo ""
+      exit 0
       ;;
     * )
-      if [ -z "$@" ]
+      if  [ !$@ ]
       then
         KUBE_CTL="kubectl"
+        echo ""
+        echo "Using default 'kubectl' command set!"
+        echo ""
+        #echo "$USAGE"
+        break
       else
+        echo ""
         echo "Invalid arguments '$@', Try:"
         echo ""
         echo "$USAGE"
+        exit 2
       fi
-      break
       ;;
   esac
 done
 
-echo "KUBE_CTL = '$KUBE_CTL'"
+echo ""
+
+eval "git config --global user.name ""PICTC"" "
+eval "git config --global user.email ""support@premier-ictc.com"" "
+
+echo "### Download Latest ArgoCD Standalone config file... \n\n"
+eval "cd ./control_plane/orchestrator && wget \"https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml\" "
+
+
+# echo "### Deploy 'ArgoCD Standalone' deployment into the 'control_plane' namespace... \n\n"
+# eval "$KUBE_CTL create namespace control_plane;"
+# eval "$KUBE_CTL apply -n ns-control_plane -f ./ns-control_plane/po-orchestrator/install.yaml"
+
+# echo "### Create Ingress rule for 'ArgoCD Server' web UI via the FQDN... \n\n"
+# eval "$KUBE_CTL create ingress --rule=\"cpanel.csp.cloud.premier-ictc.com/=svc1:80\" "
+
+# echo "### Deploy 'WhoAmI' pod into the 'control_plane' namespace... \n\n"
+# eval "$KUBE_CTL create namespace whoami"
+# eval "$KUBE_CTL apply -f ./ns-control_plane/po-testing -R"
